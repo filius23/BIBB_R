15542bd55071ce872a0111e2fc2997cc61420b4a
Andreas Filser <filser-a@gmx.de>
2022-09-27 21:17:02 +0200

    updates

---
+++ prog_prelim/02_Lösung.R
@@ -0,0 +1,14 @@
+# ------------------- #
+# Kapitel 2: data.frame
+# Lösung
+# ------------------- #
+
+dat2 <- data.frame(studs = c(14954,47269 ,23659,9415 ,38079),
+                  profs = c(250,553,438 ,150,636),
+                  prom_recht = c(FALSE,TRUE,TRUE,TRUE,FALSE),
+                  gegr  = c(1971,1870,1457,1818,1995))
+
+## c("FH Aachen","RWTH Aachen","Uni Freiburg","Uni Bonn","FH Bonn-Rhein-Sieg")
+
+## c("RWTH Aachen","Uni Freiburg","Uni Bonn","FH Aachen","FH Bonn-Rhein-Sieg")
+
