b4e0934026851823874dca1c49b1370475cee68b
Andreas Filser <filser-a@gmx.de>
2022-09-27 22:16:01 +0200

    kap03 neu

--- prog_prelim/03_desc.R
+++ prog_prelim/03_desc.R
@@ -1,108 +1,117 @@
-if(Sys.getenv("USERNAME") == "filse" ) .libPaths("D:/R-library4") 
-if(Sys.getenv("USERNAME") == "filse" ) path <- "D:/oCloud/RFS/"
-knitr::opts_chunk$set(collapse = F)
+# ------------------- #
+# Kapitel 3: Tabellen 
+# ------------------- #
+
 library(tidyverse)
- a14 <- readr::read_delim(paste0(path,"allbus_kumuliert.csv"), delim = ";", col_types = cols(.default = col_double())) %>% 
-   filter(year == 2014) # für Gini Illustration
 etb18 <- haven::read_dta("./data/BIBBBAuA_2018_suf1.0.dta")
-library(Statamarkdown)
 
-## install.packages("haven") # falls nicht schon installiert
 
-## library(haven) # datenimport für stata-datensätze
-## library(tidyverse) # tidyverse
-## etb18 <- read_dta("./data/BIBBBAuA_2018_suf1.0.dta")
-
+# table & xtabs -------------
 table(etb18$m1202)
-
 xtabs(~m1202,data=etb18)
 
+## labels anzeigen -----
 ## install.packages("labelled") # nur einmal nötig
-## library(labelled)
-## val_labels(etb18$m1202)
-
 library(labelled)
 val_labels(etb18$m1202)
-t2 <- xtabs(~m1202,data=etb18)
 
+
+## count() zeigt labels: ------
 etb18 %>% count(m1202)
 
+
+# count/table als Objekt --------
 t1 <- xtabs(~m1202,etb18)
 t2 <- etb18 %>% count(m1202)
 
+## unterschiedliche Objekte: ------
 class(t1)
 class(t2)
 
+# andere Werte ------------
+# relative Häufigkeiten
 xtabs(~m1202,data=etb18) %>% prop.table(.) 
 
+# kumulierte Werte
 xtabs(~m1202,data=etb18) %>% cumsum(.)
-ct2 <- xtabs(~m1202,data=etb18) %>% cumsum(.)
 
+# kumulierte relative Werte
 xtabs(~m1202,data=etb18) %>% prop.table() %>% cumsum()
-t2x <- cumsum(prop.table(xtabs(~m1202,data=etb18)))
 
+# Kreuztabellen 
 table(etb18$S1, etb18$m1202)
 xtabs(~S1+m1202, data = etb18)
 
+## Übung ------
+
+# Missings: NA ----------
 etb18$m1202[etb18$m1202 == -1] # nur m1202 = -1 aufrufen
 
+# mit NA überschreiben:
 etb18$m1202[etb18$m1202 == -1]  <- NA
+etb18 %>% select(m1202) %>% slice(70:80)
+etb18 %>% select(m1202) %>% filter(is.na(m1202))
 
+
+
+# xtabs() übergeht NA:
 xtabs(~m1202,data=etb18)
+xtabs(~m1202,data=etb18) %>% prop.table() %>% cumsum()
 
 xtabs(~m1202,data=etb18,addNA = T)
 
+#  count() zählt auch NA aus:
 etb18 %>% count(m1202)
-
 etb18 %>% filter(!is.na(m1202)) %>% count(m1202)
 
-## set linesize 80
 
-## qui use "D:\Datenspeicher\BIBB_BAuA/BIBBBAuA_2018_suf1.0.dta", clear
-
-## qui mvdecode m1202 F100_kldb2010_BOF F1609_kldb2010_BOF F1610_kldb2010_BOF, mv(-1)
-
-## qui mvdecode F100_wib1, mv(-4/-1)
-
-## tab m1202
+## Übung ------
 
 
-knitr::include_graphics("./pic/103_mutate.png")
+# mehrere Kennzahlen in einer Tabelle -----
 
 
 etb18 %>% 
    count(m1202) # ausgangsbefehl
 
+
+# weitere Werte als Variablen mit mutate() anfügen
 etb18 %>% 
    count(m1202) %>% 
    mutate(pct= prop.table(n)*100) # erweitert um pct
 
+# können auch noch mehr Variablen anfügen
 etb18 %>% 
    count(m1202) %>% 
    mutate(pct= prop.table(n)*100,
           Cum = cumsum(pct)) 
 
+# NAs ausschließen mit filter()
 etb18 %>% 
   filter(!is.na(m1202)) %>% 
    count(m1202) %>% 
    mutate(pct= prop.table(n)*100,
           Cum = cumsum(pct)) 
 
+## Zwei Variablen ----
 etb18 %>% 
   filter(!is.na(m1202)) %>% 
    count(m1202,S1)
 
+## relative Anteile -----
 etb18 %>% 
   filter(!is.na(m1202)) %>% 
   count(m1202,S1) %>% 
   mutate(pct= prop.table(n)*100) 
 
+## relative Anteile nach Gruppe -----
 etb18 %>% 
    filter(!is.na(m1202)) %>% 
    count(m1202,S1) %>% 
    group_by(S1) %>% 
    mutate(pct_gender = prop.table(n)) 
 
+# bestimmte Werte nachsehen
 etb18 %>% 
   filter(!is.na(m1202)) %>% 
    count(m1202,S1) %>% 
@@ -120,19 +129,24 @@ class(tab_aus_gender)
 
 tab_aus_gender %>%  filter(m1202 == 3)
 
-## etb18 %>% count(Bula,S1) # wird abgeschnitten
-## etb18 %>% count(Bula,S1) %>% print(n=Inf) # alle Werte werden gezeigt
+# print() bei langen tibbles:
+etb18 %>% count(Bula,S1) # wird abgeschnitten
+etb18 %>% count(Bula,S1) %>% print(n=Inf) # alle Werte werden gezeigt
+etb18 %>% count(Bula,S1) %>% print(n=2) # alle Werte werden gezeigt
+etb18 %>% count(Bula,S1) %>% print(n=12) # alle Werte werden gezeigt
+
+
+## Übung ------
+
+
+# Lage- & Konzentrationsmaße -------
 
 summary(etb18$F518_SUF)
-
 etb18$F518_SUF[etb18$F518_SUF %in% 99998:99999] <- NA # missings überschreiben
-
 summary(etb18$F518_SUF)
 
 mean(etb18$F518_SUF)
-
 mean(etb18$F518_SUF,na.rm = T)
-
 quantile(etb18$F518_SUF,probs = .4, na.rm = T)
 
 ## install.packages("ineq") # einmal installieren
@@ -140,15 +154,14 @@ quantile(etb18$F518_SUF,probs = .4, na.rm = T)
 library(ineq) # ineq laden
 Gini(etb18$F518_SUF)
 
+## summarise -----
 etb18 %>% summarise(Minimum = min(F518_SUF,na.rm = T),
                     Median = median(F518_SUF,na.rm = T),
                     Mittelwert = mean(F518_SUF,na.rm = T),
                     Maximum = max(F518_SUF,na.rm = T),
                     Gini = Gini(F518_SUF))
 
-nrw_nds <- mean(etb18$F518_SUF[etb18$Bula == 5], na.rm = T) -
-  mean(etb18$F518_SUF[etb18$Bula == 3], na.rm = T) # Niedersachsen
-
+## group_by() ------
 etb18 %>% 
   group_by(Bula) %>% 
   summarise(mean_inc = mean(F518_SUF, na.rm = T) )
@@ -162,23 +175,3 @@ etb18 %>%
   group_by(Bula) %>% 
   summarise(mean_inc = mean(F518_SUF, na.rm = T) )
 
-## etb18 <- read_dta("./data/BIBBBAuA_2018_suf1.0.dta")
-
-library(gt)
-etb18 %>% count(gkpol) %>% pull(gkpol) %>% labelled::val_labels() %>% enframe(name = "label") %>% gt() %>% tab_options(  table.font.size = 9)
-# %>% kable(.) %>% 
-#   kable_styling(bootstrap_options = c("striped", "hover", "condensed"), full_width = F) %>% 
-#   row_spec(0, color = "white",font_size = 0)
-
-## etb18$zpalter[etb18$zpalter>100] <- NA
-
-round(21.12121123,digits = 3)
-round(21.12121123,digits = 5)
-round(21.12121123,digits = 0)
-
-xtabs(~S1+m1202, data = etb18) %>% 
-  prop.table(.,margin = 1) %>% 
-  round(.,3)
-
-t4 <- table(etb18$zpalter)
-t4[which(t4 == max(t4))] # Modus
