90b53e262387c101cb60c5ec66227a69d516e90d
Andreas Filser <filser-a@gmx.de>
2022-10-06 16:31:32 +0200

    final render

--- prog_prelim/12_Schleifen_Funktionen.R
+++ prog_prelim/12_Schleifen_Funktionen.R
@@ -1,66 +1,105 @@
-if(Sys.getenv("USERNAME") == "filse" ) .libPaths("D:/R-library4") 
+# ------------------- #
+# Kapitel 11: Data Wrangle
+# merge/join & reshape
+# ------------------- #
+
 library(tidyverse)
 library(marginaleffects)
 
+# Daten einlesen ------ 
 etbx <-  haven::read_dta("./data/BIBBBAuA_2018_suf1.0.dta",
                          col_select = c("S1","F518_SUF","m1202","az","zpalter","F1605e")) %>% 
   filter(F518_SUF < 99998, m1202 %in% 1:4, zpalter < 9999 ) %>% 
   mutate(ausb = factor(m1202, levels = 1:4, labels = c("ohne","dual/schul.","Aufst.","FH/Uni")),
          S1 = factor(S1,levels = 1:2,labels =c("m","w")))
 
+
+# Funktion für Modelle ----- 
 mod_function <- function(modx){
   mx <- lm(formula = modx,data = etbx)
   return(mx)
 }
-
+ # anwenden 
 mod_function("F518_SUF ~ az")
 
-mlst2 <- list(
+## Modell Liste ------
+mlst <- list(
   "Modell 1" = "F518_SUF ~ az",
-  "Modell 2"  = "F518_SUF ~ az + S1",
-  "Modell 3" = "F518_SUF ~ az + S1 + m1202",
-  "Modell 4" = "F518_SUF ~ az + S1 + m1202 + zpalter"
+  "Modell 2" = "F518_SUF ~ az + S1",
+  "Modell 3" = "F518_SUF ~ az + S1 + ausb",
+  "Modell 4" = "F518_SUF ~ az + S1 + ausb + zpalter"
 )
 
-mlst2[[4]]
-mlst2$`Modell 4`
+mlst[[4]]
+mlst$`Modell 4`
 
-mods2 <- lapply(mlst2,mod_function)
-mods2$`Modell 1`
-mods2$`Modell 2`
+## Modellliste anwenden: Modellserien erstellen -----
+mods <- lapply(mlst,mod_function)
+mods$`Modell 1`
+mods$`Modell 2`
 
-modelsummary::modelsummary(mods2,stars = T,gof_omit = "IC|RM|Log")
+## Modelsummary ------
+modelsummary::modelsummary(mods,stars = T,gof_omit = "IC|RM|Log")
 
-mod_function2 <- function(modx, tidy_mod = T){
+# if/else -----------------
+
+add_controls <- c("+ zpalter + I(zpalter^2)")
+
+mod_function2 <- function(modx, add_age){
+  if(add_age == T) {
+    mx <- lm(formula = paste0(modx,add_controls),data = etbx)
+  } else {
+    mx <- lm(formula = paste0(modx),data = etbx)
+  }
+  return(mx)
+}
+
+mod_function2("F518_SUF ~ az",add_age=F)
+mod_function2("F518_SUF ~ az",add_age=T)
+
+
+
+## Regressionsergebnisse als data.frame -------
+mod_function3 <- function(modx, tidy_mod = T){
   mx <- lm(formula = modx,data = etbx)
   if(tidy_mod == T) mx <- tidy(mx,conf.int = T)
   return(mx)
 }
 
-mod_function2("F518_SUF ~ az")
-mod_function2("F518_SUF ~ az",tidy_mod = F)
+mod_function3("F518_SUF ~ az") # output ist jetzt ein data.frame
+mod_function3("F518_SUF ~ az",tidy_mod = F)
 
-mod_l2 <- lapply(mlst2,mod_function2)
-lapply(mod_l2,class)
+# als Objekt ablegen 
+mod_l3  <- lapply(mlst,mod_function3)
+lapply(mod_l3 ,class)
+bind_rows(mod_l3 ,.id="Mod_name")
 
-## bind_rows(mod_l2,.id="Mod_name")
+# alles auf einmal:
+lapply(mlst,mod_function3) %>% bind_rows(.id="Mod_name")
 
-bind_rows(mod_l2,.id="Mod_name") %>% 
-  rmarkdown::paged_table()
+# mit purrr:
+# map_dfr(mlst,mod_function3,.id="Mod_name")
 
+
+
+
+
+# adhoc function ------
 mods3 <- lapply(mlst2,function(modx){
   mx <- lm(formula = modx,data = etbx)
   return(mx)
 })
 mods3$`Modell 1`
 
+# for loops ------
 for(i in 1:8){
   print(i)
 }
 
-## for(i in 1:8){
-##   etbx %>% slice(i) %>% print()
-## }
+## beispiel 2
+for(i in 1:8){
+ etbx %>% slice(i) %>% print()
+}
 
 etb_ue12 <- 
   haven::read_dta("./data/BIBBBAuA_2018_suf1.0.dta",
@@ -69,7 +108,13 @@ etb_ue12 <-
   mutate(ausb = factor(m1202, levels = 1:4, labels = c("ohne","dual/schul.","Aufst.","FH/Uni")),
          S1 = factor(S1,levels = 1:2,labels =c("m","w")))
 
+
+# map = lapply ------
 mods <- map(mlst2,~lm(formula = .x,data = etbx))
+
+# _dfr = "data.frame" mit row_bind()
+map_dfr(mlst2,~lm(formula = .x,data = etbx) %>% tidy(.),.id = "mod")
+
 
 for(v in c("ausb","S1","F1605e")){
   etbx %>% count(v) %>% print()
