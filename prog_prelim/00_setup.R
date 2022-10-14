f00f61f1de99f7325f12f6578184a476a21b5508
Andreas Filser <filser-a@gmx.de>
2022-10-14 11:48:06 +0200

    prog drp

--- prog_prelim/00_setup.R
+++
@@ -1,9 +0,0 @@
-
-knitr::purl(input = "06_data_wrangle2.qmd",
-            output = "./prog_prelim/06_data_wrangle2.R",documentation = 0)
-
-rstudioapi::navigateToFile("./prog_prelim/06_data_wrangle2.R")
-
-
-
-if(Sys.getenv("USERNAME") == "filse" ) .libPaths("D:/R-library4") 
